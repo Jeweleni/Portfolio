49be2e4e85f2e837d0844216b2a48ab7aca1c8aa
Eni Momoreoluwa Jewel <109070607+Jeweleni@users.noreply.github.com>
2023-11-04 20:43:29 +0100

    resume

--- src/Components/CTA/CTA.js
+++ src/Components/CTA/CTA.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Resume from "./Resume.pdf"
+import Resume1 from "./Resume.pdf"
 
 const CTA = () => {
     return (  
         <div className="cta">
-            <a href={Resume} className="btn" >View Resume</a>
+            <a href={Resume1} className="btn" >View Resume</a>
             {/* <a href="contact" className="btn btn-primary">Let's Talk</a> */}
         </div>
     );
