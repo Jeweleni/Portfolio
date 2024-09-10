3714a2c4505171c8a1b726fd4f7002603629bdfa
Eni Momoreoluwa Jewel <109070607+Jeweleni@users.noreply.github.com>
2024-09-10 16:30:55 +0100

    cta

--- src/Components/CTA/CTA.js
+++ src/Components/CTA/CTA.js
@@ -5,7 +5,7 @@ const CTA = () => {
     return (  
         <div className="cta">
             <a href ={resume} className="btn" >View Resume</a>
-            {/* <a href="contact" className="btn btn-primary">Let's Talk</a> */}
+            
         </div>
     );
 }
